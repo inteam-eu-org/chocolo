8988846c2a97eda12b3782df2670ad90b09e5372
Chocorean <chocorean@pm.me>
2024-06-03 23:03:41 -0400

    fix proc and log msg

--- config/initializers/ip_address.rb
+++ config/initializers/ip_address.rb
@@ -1,5 +1,6 @@
 Rails.application.config.after_initialize do
-  warn "\nThis is your IP: #{Socket.ip_address_list.find { |ai| ai.ipv4? && !ai.ipv4_loopback? }.ip_address}"
+  ip = Socket.ip_address_list.find { |ai| ai.ipv4? && !ai.ipv4_loopback? }.ip_address
+  warn "\nThis is your IP: #{ip}"
   warn "Use it to access the app from your mobile device on the same network"
-  warn "For instance: http://192.168.0.1:3000\n\n"
+  warn "For instance: http://#{ip}:3000\n\n"
 end
