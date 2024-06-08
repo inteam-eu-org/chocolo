e41ae383988e75de1cb57e0342001099c902089a
CedricBell <cedric.bell@telecomnancy.net>
2024-06-08 09:47:35 -0400

    PWA a l'air de fonctionner

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,4 +6,15 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 
+document.addEventListener('DOMContentLoaded', () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  }
+});
+
 export { application }
